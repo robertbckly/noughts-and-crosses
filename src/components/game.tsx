75d59a22735cb3e28ecefaba90b7c39f72f274d9
Rob Buckley <robertbckly@gmail.com>
2024-04-07 16:36:40 +0100

    couple square size and text size

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { SIZE } from '../constants/constants';
 import { useGameLogic } from '../hooks/use-game-logic';
 import { getCoordsFromIndex } from '../utils/utils';
@@ -21,6 +22,23 @@ export function Game() {
     handleReset,
   } = useGameLogic();
 
+  const [squareSize, setSquareSize] = useState(0);
+  const lastSquareRef = useRef<HTMLButtonElement | null>(null);
+
+  // Track square size in order to adjust font size
+  useEffect(() => {
+    let resizeObserver: ResizeObserver;
+    const squareToMeasure = lastSquareRef?.current;
+    if (squareToMeasure) {
+      resizeObserver = new ResizeObserver((entries) => {
+        setSquareSize(entries[0]?.contentBoxSize[0]?.blockSize || 0);
+      });
+      resizeObserver.observe(squareToMeasure);
+    }
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
   return (
     <div className="m-auto h-full max-w-lg p-4">
       <aside className="mb-8 mt-4 flex items-start">
@@ -41,9 +59,11 @@ export function Game() {
           Reset
         </button>
       </aside>
-      <main className="flex flex-wrap border border-white">
+      <main className="flex flex-wrap border-2 border-white">
         {board.map((square, index) => (
           <button
+            // Add ref to last square
+            ref={index === board.length - 1 ? lastSquareRef : undefined}
             // Board squares will never change position in array.
             // eslint-disable-next-line react/no-array-index-key
             key={index}
@@ -53,10 +73,10 @@ export function Game() {
             aria-label={`Square 
               ${getCoordsFromIndex(Number(index))}:
               ${square || 'empty'}`}
-            className="aspect-square flex-shrink-0 overflow-hidden border border-white font-bold transition-colors"
+            className="aspect-square flex-shrink-0 overflow-hidden border-2 border-white font-bold leading-none"
             style={{
               width: `${(1 / SIZE) * 100}%`,
-              fontSize: `${10 / SIZE}rem`,
+              fontSize: `${squareSize * 0.8}px`,
               backgroundColor: winnerInfo?.line.squareIndexes.includes(index)
                 ? 'rgb(100, 125, 255)'
                 : undefined,
